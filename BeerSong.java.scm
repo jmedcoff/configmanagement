791fa49a6471db6dc6b562e338b775f1e868bb81
Jason Medcoff <jemedcoff@oakland.edu>
2017-01-29 14:53:30 -0500

    Implemented functions main and Ninety_Nine_Bottles_of_Beer

--- BeerSong.java.scm
+++ BeerSong.java.scm
@@ -1,5 +1,22 @@
 #lang racket
-(define (main) "unimplemented")
+(define (main) (
+                Ninety_Nine_Bottles_of_Beer 99))
 
-(define (Ninety_Nine_Bottles_of_Beer num) "unimplemented")
+(define (Ninety_Nine_Bottles_of_Beer num) 
+                                           (if
+                                             (= num 1)
+                                             (string-append "One bottle of beer on the wall, "
+                                                            "one bottle of beer. "
+                                                            "Knock one down, pass it around, "
+                                                            "no more bottles of beer on the wall. ")
+                                             (string-append (number->string num) " bottles of beer on the wall, "
+                                                            (number->string num) " bottles of beer. "
+                                                            " Knock one down, pass it around, "
+                                                            (number->string (- num 1))
+                                                            " bottle"
+                                                            (if (= num 2)
+                                                                ""
+                                                                "s")
+                                                                " of beer on the wall. "
+                                                            (Ninety_Nine_Bottles_of_Beer (- num 1)))))
 
